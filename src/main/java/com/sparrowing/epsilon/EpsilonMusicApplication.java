bcce4daee80681820218a9ce6184a0237c0b310f
Sparrow <mel.e.grigg@gmail.com>
2017-10-06 01:38:22 -0500

    Initial project commit

---
+++ src/main/java/com/sparrowing/epsilon/EpsilonMusicApplication.java
@@ -0,0 +1,12 @@
+package com.sparrowing.epsilonmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsilonMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpsilonMusicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sparrowing/epsilonmusic/EpsilonMusicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sparrowing.epsilonmusic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EpsilonMusicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
